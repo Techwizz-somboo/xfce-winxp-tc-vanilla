b061ca0640eec481181a311fc129207fffb02a15
Techwizz <paulrblack.prb@gmail.com>
2025-08-10 23:25:13 +0000

    Attempted deb install fix.

--- includes.container/deb-pkgs/install-debs.sh
+++ includes.container/deb-pkgs/install-debs.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 set -e
-for file in /deb-pkgs/*; do
-    if [ ! -f "$file" ]; then
-        continue
-    fi
-
-    extension=${file##*.}
-    if [ $extension != "deb" ]; then
-        continue
-    fi
-
-    echo "installing $file"
-
-    apt-get install -y $file
-done
+apt-get install -y /deb-pkgs/*.deb
